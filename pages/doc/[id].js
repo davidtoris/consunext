29eb6a983dc120e705a24ab0e0f09efad0542e50
“davidtoris” <edavid.toris@gmail.com>
2021-11-05 11:15:22 -0600

    use router id

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -7,9 +7,12 @@ const Post = () => {
     const { id } = router.query
     
     const getSpecialities = async ()=> {
-        const res = await fetch(`${API}/especialidades`);
+
+        const res = await fetch(`${API}/especialidades/${id}`);  
         const data = await res.json()
-        const detalle = data.detalle
+        const detalle = data.detalle[0]
+
+        
 
         console.log(detalle)
         }

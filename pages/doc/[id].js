2d294f3b40e156e16fef2a598552a5ae6bd75930
“davidtoris” <edavid.toris@gmail.com>
2021-11-05 11:02:13 -0600

    use router

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,8 +1,27 @@
+import axios from 'axios';
 import { useRouter } from 'next/router'
 
+
+
 const Post = () => {
-  const router = useRouter()
-  const { id } = router.query
+    const router = useRouter()
+    const { id } = router.query
+    
+    const getSpecialities = async ()=> {
+        await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then( resp => {  
+            
+            console.log(resp);
+            console.log(id);
+            console.log("antes")
+            // setSpeciality(res.data.detalle[0]);
+            })
+            .catch( err => {
+            console.log("NO se pudo",err);
+            })
+        }
+
+        getSpecialities()
 
   return <p>Post: {id}</p>
 }

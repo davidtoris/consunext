47aee605b273a0a37e29fca016390a667af5bb45
“davidtoris” <edavid.toris@gmail.com>
2021-11-04 21:41:15 -0600

    algo function

--- pages/algo/[id].js
+++ pages/algo/[id].js
@@ -1,9 +1,38 @@
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    const data = await res.json()
+    
+    console.log(res)
+    console.log(data)
+
+    const paths = data.map (algo => {
+        return {
+            params : { id: algo.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+} 
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id );
+    const data = await res.json()
+
+    return {
+        props : { algo : data }
+    }
+} 
 
 
-function Details() {
+
+function Details({ algo }) {
     return (
-        <div>
-            Details page
+        <div className="text-5xl">
+            {algo.title}
         </div>
     )
 }

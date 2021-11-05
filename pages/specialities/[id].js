770b33f9f91e534defd02f6bc48aae6eecfe5e59
“davidtoris” <edavid.toris@gmail.com>
2021-11-05 08:53:33 -0600

    use context

--- pages/specialities/[id].js
+++ pages/specialities/[id].js
@@ -1,16 +1,20 @@
 import {API} from "../../api"
 
+export const getServerSideProps = async (context) => {
 
-export const getServerSideProps = async () => {
-  const res = await fetch(`${API}/especialidades/1`);
+  const id = context.params.id;
+  const res = await fetch(`${API}/especialidades/${id}`);  
   const data = await res.json()
   const detalle = data.detalle
+
+//   const res = await fetch(`${API}/especialidades/${context}`);
+//   const data = await res.json()
+//   const detalle = data.detalle
   return { props : { detalle } }
 } 
 
 
 function Details({ detalle }) {
-
     console.log(detalle)
     return (
         // <h1>hola</h1>

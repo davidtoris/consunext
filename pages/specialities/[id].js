cd7880f528d8ffc169db0157f2eaa7bf949c1336
“davidtoris” <edavid.toris@gmail.com>
2021-11-05 10:55:32 -0600

    id

--- pages/specialities/[id].js
+++ pages/specialities/[id].js
@@ -1,26 +1,19 @@
 import {API} from "../../api"
 
 export const getServerSideProps = async (context) => {
-
   const id = context.params.id;
   const res = await fetch(`${API}/especialidades/${id}`);  
   const data = await res.json()
   const detalle = data.detalle
 
-//   const res = await fetch(`${API}/especialidades/${context}`);
-//   const data = await res.json()
-//   const detalle = data.detalle
   return { props : { detalle } }
 } 
-
 
 function Details({ detalle }) {
     console.log(detalle)
     return (
-        // <h1>hola</h1>
         <div className="text-5xl">
             {detalle.map( (item, index) => (
-                
                 <div key={index}>{item.especialidad}</div>
             ))}
             
